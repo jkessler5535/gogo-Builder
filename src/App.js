b778fe123874420472a787aeac6dbfa8dd5820a5
JamieKessler <jkessler5535@gmail.com>
2024-05-29 16:32:39 -0700

    added buttons/logo & changed background color

--- src/App.js
+++ src/App.js
@@ -1,57 +1,43 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.scss";
 import AvatarBuilder from "./components/gogo-builder";
-import html2canvas from "html2canvas";
+
 
 //Generator
 import TVGenerator from "./assets/lab-assets/tv-generator.svg";
 import Generator from "./assets/lab-assets/generator.svg";
 import PanelLines from "./assets/lab-assets/panel-lines.svg";
-import { motion } from "framer-motion";
+
 
 //Logo
-import Logo from "./assets/gogobuilder-logo.svg";
 import Fire from "./assets/icons/fire.svg";
+import Logo2 from "./Images/Logo-2.png";
+import Layer1 from "./Images/layer1.png";
+import Layer2 from "./Images/layer2.png";
+import Button1 from "./Images/Button1.png";
+import Button2 from "./Images/Button2.png";
 
 function App() {
-  const downloadAvatar = async () => {
-    const avatarElement = document.querySelector(".avatar-only");
-    const canvas = await html2canvas(avatarElement, {
-      scale: 2,
-      useCORS: true,
-      backgroundColor: null,
-    });
-
-    const link = document.createElement("a");
-    link.href = canvas.toDataURL("image/png");
-    link.download = "avatar.png";
-    link.click();
-  };
+  
+    
   return (
     <div className="app-container">
       <header className="App-header">
-        <img src={Logo} alt="GogoBuilder Logo" />
+        <img src={Logo2} alt="GogoBuilder head"   />
+        <img src={Layer1} alt="gogo letters"/>
+        <img src={Layer2} alt="gogo letters"/>
+        
         <nav>
           <div className="nav-list">
             <a href="https://app.gogopool.com/liquid-staking/">Liquid Stake</a>
-            <a href="https://app.gogopool.com/">Validate</a>
+            <a href="https://app.gogopool.com/">Validate AVAX</a>
             <a href="https://docs.gogopool.com/gogopool-primer">
-              Our Mission
+              Explore Subnets
             </a>
           </div>
-          <motion.button
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            style={{
-              border: "none",
-              backgroundColor: "transparent",
-            }}
-          >
-            <button className="btn" onClick={downloadAvatar}>
-              Download Avatar
-            </button>
-          </motion.button>
+          <img src={Button1} alt="gogo letters"/>
+          <img src={Button2} alt="gogo letters"/>
+          
         </nav>
       </header>
       <div className="builder-container">
